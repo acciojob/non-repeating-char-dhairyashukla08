0c8d00246ab888db648189359c02f90d0b4d8749
Dhairya Shukla <113496448+dhairyashukla08@users.noreply.github.com>
2025-11-29 15:20:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+ if (!str || str.length === 0) {
+        return null;
+    }
+	 const charCounts = {};
+	 for (let i = 0; i < str.length; i++) {
+        const char = str[i];
+		   charCounts[char] = (charCounts[char] || 0) + 1;
+    }
+	for (let i = 0; i < str.length; i++) {
+        const char = str[i];
+		if (charCounts[char] === 1) {
+            return char;
+        } 
+		
+		return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
+	
